f6574436a3f75d57a8f7488c4f06af49f8916300
billystein25 <59233918+billystein25@users.noreply.github.com>
2025-11-24 18:00:23 +0000

    Update EnemyFloatingSpike comments

--- scenes/entities/enemy_floating_spike/enemy_floating_spike.gd
+++ scenes/entities/enemy_floating_spike/enemy_floating_spike.gd
@@ -1,6 +1,9 @@
 class_name EnemyFloatingSpike
 extends Area2D
 
+# We can define our own signals like this. Inside the parenthesis we set any attributes
+# that we want to pass with the signal. Similar to how the body_entered signal from
+# Area2D passes the body that entered the area.
 signal hit_player(me: Node2D)
 
 # Called when the node enters the scene tree for the first time.
@@ -8,4 +11,7 @@ func _ready() -> void:
 	body_entered.connect(_on_body_entered)
 
 func _on_body_entered(_body: Node2D) -> void:
+	# We can emit any signal of this class like this. For the attributes of the emit()
+	# function we pass how many attributes the signal needs to pass. In our case we
+	# have one attribute: 'me' of type Node2D.
 	hit_player.emit(self)
